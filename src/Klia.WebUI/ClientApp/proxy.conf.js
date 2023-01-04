1f64769fc7c389053fe3e14ca11b72726f726dff
andude10 <indko.06@gmail.com>
2023-01-04 22:20:15 +0300

    change localhost to 0.0.0.0 to follow docker ports mapping

--- src/Klia.WebUI/ClientApp/proxy.conf.js
+++ src/Klia.WebUI/ClientApp/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:24450';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://0.0.0.0:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://0.0.0.0:44480/';
 
 const PROXY_CONFIG = [
   {
